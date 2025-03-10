8fa542cc2f9d3d2d4164da4ced77116013ef09b3
tanvirGHD <155833719+tanvirGHD@users.noreply.github.com>
2025-03-10 20:40:43 +0600

    Project Complete

---
+++ script.js
@@ -0,0 +1,9 @@
+
+// JavaScript to toggle FAQ answers
+const faqItems = document.querySelectorAll('.faq-item');
+
+faqItems.forEach(item => {
+    item.addEventListener('click', () => {
+        item.classList.toggle('active');
+    });
+});
